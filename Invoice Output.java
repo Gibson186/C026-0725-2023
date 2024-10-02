468fa2486bad6a088a9d8f3dcfa60397305bc9fe
Gibson186 <muriuki.mwangi23@students.dkut.ac.ke>
2024-10-02 23:56:53 +0300

    Create Invoice Output.java

---
+++ Invoice Output.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****************C026-01-0725/2023********************");
+        System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City * Tel. No:123-4567");
+        System.out.println("\tVAT REG. TIN 098-765-4321-0000");
+        System.out.println("\t\tSALES INVOICE\n");
+        System.out.println("Sold to: Gibson Mwangi Muriuki \t\t\t Date:28/04/2020");
+        System.out.println("Address:private bag \t\t\t Contact Number: 0100100100\n\n");
+        System.out.println("Qty\t\tItem Description\t\tUnit Price\t\tAmount");
+        System.out.println("4 \t\tPencil\t\t20\t\t80.00");
+        System.out.println("10\t\tDuster\t\t50\t\t500.00");
+        System.out.println("9\t\tPens\t\t30\t\t270.00");
+        System.out.println("10\t\tcrayon\t\t80\t\t800.00");
+        System.out.println("\t\t\t\t Sub Total \t\t 1650.00");
+        System.out.println("\t\t\t\t VAT(16%) \t\t 264.00");
+        System.out.println("\t\t\t\t Total Amount \t\t 1914.00");}}
+
+
+
